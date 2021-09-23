629b7fa4667438452b58166e1f02e93c30ad9ae1
Jefferson <jefferson_alvespereira@yahoo.com.br>
2021-09-22 23:43:30 -0300

    Adding test for withdraw events

--- BankApplication/AccountCommands/WithdrawCommand.cs
+++ BankApplication/AccountCommands/WithdrawCommand.cs
@@ -11,7 +11,7 @@ namespace BankApplication.AccountCommands
         {
             if (accountRepository.Exist(accountEvent.Origin))
             {
-                DepositValue(accountRepository, accountEvent);
+                WithdrawtValue(accountRepository, accountEvent);
             }
             else
             {
@@ -19,52 +19,10 @@ namespace BankApplication.AccountCommands
             }
         }
 
-        private void DepositValue(IAccountRepository accountRepository, AccountEvent accountEvent)
+        private void WithdrawtValue(IAccountRepository accountRepository, AccountEvent accountEvent)
         {
-            var balance = accountRepository.Get(accountEvent.Destination);
 
-            var newBalanceValue = accountEvent.Amount + balance.balance_value;
-
-            accountRepository.UpdateBalance(accountEvent.Destination, new Domain.Entities.Balance()
-            {
-                balance_value = newBalanceValue
-            });
-
-            var result = new CommandResponse()
-            {
-                Deposit = new DepositResponse()
-                {
-                    destination = new Destination()
-                    {
-                        id = accountEvent.Destination,
-                        balance = newBalanceValue
-                    }
-                }
-            };
-
-            this.Result = result.Deposit;
         }
 
-        private void CreateAccountWithInitialBalance(IAccountRepository accountRepository, AccountEvent accountEvent)
-        {
-            accountRepository.Create(accountEvent.Destination, new Domain.Entities.Balance()
-            {
-                balance_value = accountEvent.Amount
-            });
-
-            var result = new CommandResponse()
-            {
-                Deposit = new DepositResponse()
-                {
-                    destination = new Destination()
-                    {
-                        id = accountEvent.Destination,
-                        balance = accountEvent.Amount
-                    }
-                }
-            };
-
-            this.Result = result.Deposit;
-        }
     }
 }
---
+++ TestBankApi/WithdrawTest.cs
@@ -0,0 +1,48 @@
+﻿using BankApplication;
+using BankApplication.AccountCommands;
+using BankApplication.Response;
+using BankInfrastructure;
+using System;
+using System.Collections.Generic;
+using Xunit;
+using FluentAssertions;
+using static FluentAssertions.FluentActions;
+
+namespace TestBankApi
+{
+    public class WithdrawTest: IDisposable
+    {
+        private readonly AccountInvoker accountInvoker;
+        private readonly BalanceRepository balanceRepositoy;
+
+        public WithdrawTest()
+        {
+            var dataBucket = new DataBucket();
+            balanceRepositoy = new BalanceRepository(dataBucket);
+            var accountRepositoy = new AccountRepository(balanceRepositoy);
+            var accountReceiver = new AccountReceiver(accountRepositoy);
+            accountInvoker = new AccountInvoker(accountReceiver);
+        }
+
+        [Fact]
+        public void WithdrawAccountNotExistShouldFailTest()
+{
+            var account_event = new BankApplication.AccountCommands.Helper.AccountEvent()
+            {
+                Origin = "1",
+                Type = "withdraw",
+                Amount = 10
+            };
+
+            accountInvoker.SetCommand(account_event);
+
+            Invoking(() => accountInvoker.ExecuteCommand()).Should().Throw<KeyNotFoundException>();
+        }
+
+        public void Dispose()
+        {
+            balanceRepositoy.Dispose();
+        }
+
+    }
+}
